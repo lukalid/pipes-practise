5f6c28a545aa7c2f3b2ae799116ac5a4a5d5b556
lmiletic <l.miletic@msgnetconomy.net>
2019-06-18 16:21:40 +0200

    Added Filter Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  filteredStatus = '';
   servers = [
     {
       instanceType: 'medium',
@@ -38,5 +39,13 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
   }
 }
